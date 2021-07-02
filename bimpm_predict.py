432b878931afc6e2625b93974587e95e11bcc0d0
boazfr <boazfr@gmail.com>
2021-07-02 18:11:48 +0300

    ongoing

--- bimpm_predict.py
+++ bimpm_predict.py
@@ -19,13 +19,19 @@ if __name__ == '__main__':
     test_data = read_data(test, model.char2index, model.word2index, model.ukword2index, False)
     criterion = nn.CrossEntropyLoss()
 
+    word_pad_idx = len(model.word2index) + len(model.ukword2index) + 1
+
+    model.word2index = None
+    model.char2index = None
+    model.ukword2index = None
+
     with torch.no_grad():
         model.eval()
         running_loss = 0.0
         correct = 0
         dev_indices = np.array(range(len(test_data[0])))
         for i in range(len(test_data[0])):
-            batch = create_batch(test_data, dev_indices, i, i + 1, len(model.word2index) + len(model.ukword2index) + 1)
+            batch = create_batch(test_data, dev_indices, i, i + 1, word_pad_idx)
             chars_sen1 = batch[:][0].to(device)
             words_sen1 = batch[:][1].to(device)
             chars_sen2 = batch[:][2].to(device)
